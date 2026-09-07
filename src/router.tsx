894eea898def4d9855306b0aaa45c87303850141
Srinu <chalamalasrinu2003@gmail.com>
2026-09-07 13:12:59 +0530

    af

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
-    scrollRestoration: true,
+    scrollRestoration: true, 
     defaultPreload: "intent",
     defaultPreloadStaleTime: 0,
   });
